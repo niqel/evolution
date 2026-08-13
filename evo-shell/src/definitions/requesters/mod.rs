5140732057d0950eaf399252966e28b0e456de5c
Gustavo Meléndez Villarreal <niqel504@hotmail.com>
2026-08-13 12:52:33 -0600

    feat: add projected record requester

--- evo-shell/src/definitions/requesters/mod.rs
+++ evo-shell/src/definitions/requesters/mod.rs
@@ -6,6 +6,7 @@ pub mod delete_requester;
 pub mod enumerate_filesystem_requester;
 pub mod filesystem_item_requester;
 pub mod move_requester;
+pub mod record_requester;
 pub mod rename_requester;
 pub mod scope_requester;
 pub mod transfer_progress_requester;
--- evo-shell/tests/requesters/mod.rs
+++ evo-shell/tests/requesters/mod.rs
@@ -1,2 +1,3 @@
 pub mod enumerate_filesystem_requester;
 pub mod filesystem_item_requester;
+pub mod record_requester;
