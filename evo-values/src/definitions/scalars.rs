9bd94297d2101ef1eb0d1f45f725cfcf9b947bcf
Gustavo Meléndez Villarreal <niqel504@hotmail.com>
2026-09-04 23:23:45 -0600

    evo-values: restrict semantic scalar derives

--- evo-values/src/definitions/scalars.rs
+++ evo-values/src/definitions/scalars.rs
@@ -1,15 +1,15 @@
 #[repr(transparent)]
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct TextPosition(pub usize);
 
 #[repr(transparent)]
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct TextLength(pub usize);
 
 #[repr(transparent)]
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct ShiftAmount(pub u32);
 
 #[repr(transparent)]
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct PowerExponent(pub u32);
