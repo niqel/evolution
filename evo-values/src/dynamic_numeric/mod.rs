1f71893039d49d4613e4117c2a04f4e12c46b965
Gustavo Meléndez Villarreal <niqel504@hotmail.com>
2026-09-06 16:41:15 -0600

    evo-values: implement dynamic remainder

--- evo-values/src/dynamic_numeric/mod.rs
+++ evo-values/src/dynamic_numeric/mod.rs
@@ -4,12 +4,14 @@ pub mod add;
 pub mod divide;
 pub mod multiply;
 pub mod negate;
+pub mod remainder;
 pub mod subtract;
 
 pub use add::*;
 pub use divide::*;
 pub use multiply::*;
 pub use negate::*;
+pub use remainder::*;
 pub use subtract::*;
 
 pub use crate::definitions::dynamic_numeric::*;
